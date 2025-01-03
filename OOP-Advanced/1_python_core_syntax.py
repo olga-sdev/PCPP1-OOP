4716e88e0fb9d85ea015f8d9a99920fc25362a2a
Olha Rukshyna <olga.gitlab@ftml.net>
2025-01-03 16:02:39 +0200

    Add operators

--- OOP-Advanced/1_python_core_syntax.py
+++ OOP-Advanced/1_python_core_syntax.py
@@ -9,6 +9,45 @@ indexing, slicing, subscripting;
 built-in functions like str(), len()
 reflexion – isinstance(), issubclass()
 and a few more elements.
+
+Comparison methods
+Function or operator	Magic method	Implementation meaning or purpose
+==	__eq__(self, other)	equality operator
+!=	__ne__(self, other)	inequality operator
+<	__lt__(self, other)	less-than operator
+>	__gt__(self, other)	greater-than operator
+<=	__le__(self, other)	less-than-or-equal-to operator
+>=	__ge__(self, other)	greater-than-or-equal-to operator
+
+Numeric methods
+Unary operators and functions
+Function or operator	Magic method	Implementation meaning or purpose
++	__pos__(self)	unary positive, like a = +b
+-	__neg__(self)	unary negative, like a = -b
+abs()	__abs__(self)	behavior for abs() function
+round(a, b)	__round__(self, b)	behavior for round() function
+
+Common, binary operators and functions
+Function or operator	Magic method	Implementation meaning or purpose
++	__add__(self, other)	addition operator
+-	__sub__(self, other)	subtraction operator
+*	__mul__(self, other)	multiplication operator
+//	__floordiv__(self, other)	integer division operator
+/	__div__(self, other)	division operator
+%	__mod__(self, other)	modulo operator
+**	__pow__(self, other)	exponential (power) operator
+
+Augumented operators and functions
+By augumented assignment we should understand a sequence of unary operators and assignments like a += 20
+
+Function or operator	Magic method	Implementation meaning or purpose
++=	__iadd__(self, other)	addition and assignment operator
+-=	__isub__(self, other)	subtraction and assignment operator
+*=	__imul__(self, other)	multiplication and assignment operator
+//=	__ifloordiv__(self, other)	integer division and assignment operator
+/=	__idiv__(self, other)	division and assignment operator
+%=	__imod__(self, other)	modulo and assignment operator
+**=	__ipow__(self, other)	exponential (power) and assignment operator
 """
 
 
