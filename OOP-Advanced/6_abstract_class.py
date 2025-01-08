3dbdd59e3d18ec8eb80f02dea4da88ec373d99e7
olga-sdev <olga.github@airpost.net>
2025-01-08 23:18:52 +0200

    Create 6_abstract_class.py

---
+++ OOP-Advanced/6_abstract_class.py
@@ -0,0 +1,31 @@
+"""
+Abstract class is a blueprint for other classes - contract between a class designer and a programmer.
+
+Class designer sets requirements regarding methods that must be implemented by just declaring them.
+An abstract method is a method that has a declaration, but does not have any implementation.
+
+The programmer delivers the method definitions by overriding the method declarations received from the class designer.
+
+This contract assures you that a child cls will be equipped with a set of concrete methods imposed by the abstract cls.
+
+Python has a module which provides the helper class for defining Abstract Base Classes (ABC) -> abc.
+"""
+
+import abc
+from datetime import datetime
+
+
+class Clock(abc.ABC):
+    @abc.abstractmethod
+    def show_time(self):
+        pass
+
+
+class ChimingClock(Clock):
+    def show_time(self):
+        print(datetime.now().strftime('%H:%M:%S'))
+
+
+ChimingClock().show_time()
+
+# 23:17:12
