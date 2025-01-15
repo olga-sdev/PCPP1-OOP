08eb5b81b80fa1c73d6553df73d0d9d2bbb36f0a
olga-sdev <olga.github@airpost.net>
2025-01-15 23:50:55 +0200

    Create 9_inheriting_properties.py

---
+++ OOP-Advanced/9_inheriting_properties.py
@@ -0,0 +1,8 @@
+"""
+Inheriting properties from built-in classes
+Python gives you the ability to create a class that inherits properties from any Python built-in class in order to get
+a new class that can enrich the parent's attributes or methods.
+As a result, your newly-created class has the advantage of all of the well-known functionalities inherited from its
+parent or even parents and you can still access those attributes and methods.
+"""
+
