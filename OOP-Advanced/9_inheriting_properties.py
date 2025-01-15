0722913f6b1e45c9d639aeff1258eff3fb08e955
olga-sdev <olga.github@airpost.net>
2025-01-16 01:37:19 +0200

    Update 9_inheriting_properties.py

--- OOP-Advanced/9_inheriting_properties.py
+++ OOP-Advanced/9_inheriting_properties.py
@@ -6,3 +6,42 @@ As a result, your newly-created class has the advantage of all of the well-known
 parent or even parents and you can still access those attributes and methods.
 """
 
+
+class RemoveDataStructures(list):
+
+    @staticmethod
+    def check_value_type(value):
+        if not isinstance(value, list) and not isinstance(value, dict) and not isinstance(value, tuple):
+            raise ValueError('Not a boolean type')
+
+    def remove(self, value):
+        RemoveDataStructures.check_value_type(value)
+        list.remove(self, value)
+
+
+list_of_values = RemoveDataStructures()
+list_of_values.append(1)
+list_of_values.append('a')
+list_of_values.append(['1', '2', '3'])
+list_of_values.append('b')
+list_of_values.append(False)
+list_of_values.append({'key': 'value'})
+list_of_values.append((1, 2, 3))
+
+print(f'The initial list: {list_of_values}')
+
+for item in list_of_values[:]:
+    try:
+        list_of_values.remove(item)
+    except ValueError as val_err:
+        print(f'The {item} is a {type(item)}')
+
+
+print(f'The updated list: {list_of_values}')
+
+# [1, 'a', ['1', '2', '3'], 'b', False, {'key': 'value'}, (1, 2, 3)]
+# The 1 is a <class 'int'>
+# The a is a <class 'str'>
+# The b is a <class 'str'>
+# The False is a <class 'bool'>
+# [1, 'a', 'b', False]
