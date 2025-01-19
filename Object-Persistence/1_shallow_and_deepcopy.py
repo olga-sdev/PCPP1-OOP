cf01d1b87fabd37c23c0107e3d5062147b3866db
olga-sdev <olga.github@airpost.net>
2025-01-19 08:42:58 +0200

    Update 1_shallow_and_deepcopy.py

--- Object-Persistence/1_shallow_and_deepcopy.py
+++ Object-Persistence/1_shallow_and_deepcopy.py
@@ -1,3 +1,32 @@
 """
-TBD
+Variables - fundamental elements that allow us to cope with objects.
+
+An object my_list is created in the computer's memory -> has its identity;
+The object is populated with other objects -> has a value;
+A variable (label or name binding) is created -> distinct place in the computer memory.
+
+The built-in id() function returns the 'identity' of an object -> address of the object in the memory.
+This is an integer which is guaranteed to be UNIQUE and constant for this object during its lifetime.
+Two objects with non-overlapping lifetimes MAY have the SAME id() value.
+
+If 2 vars referring to the same object -> id() function is the same.
+
+'==' -> value equality
+'is' -> identity equality
 """
+
+my_list_1 = ['Data', None, 1]
+my_list_2 = ['Data', None, 1]
+my_list_3 = my_list_2
+
+print(f'id of my_list_1 is {id(my_list_1)}')
+print(f'id of my_list_2 is {id(my_list_2)}')
+print(f'id of my_list_3 is {id(my_list_3)}')
+print(f'values for lists are equal: {my_list_1 == my_list_2}')
+print(f'identities for lists are equal: {my_list_2 == my_list_3}')
+
+# id of my_list_1 is 1841391255936
+# id of my_list_2 is 1841391211072
+# id of my_list_3 is 1841391211072
+# values for lists are equal: True
+# identities for lists are equal: True
