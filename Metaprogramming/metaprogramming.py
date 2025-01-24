875fb8b6e3d96cfec0be501c49fa65c84866e329
olga-sdev <olga.github@airpost.net>
2025-01-24 22:55:00 +0200

    Create metaprogramming.py

---
+++ Metaprogramming/metaprogramming.py
@@ -0,0 +1,24 @@
+"""
+Metaprogramming is a technique in which computer programs have the ability to modify their own or other programs’ codes.
+
+n many cases metaprogramming allows programmers to minimize the number of lines of code to express a solution,
+in turn reducing development time.
+
+But the truth is that this technique could be used for tool preparation; those tools could be applied to your code
+to make it follow specific programming patterns, or to help you create a coherent API.
+
+Another example of metaprogramming is the metaclass concept,
+which is one of the most advanced concepts presented in this course.
+
+metaclasses redirect class instantiations to dedicated logic, contained in metaclasses.
+Metaclasses are applied when class definitions are read to create classes, well before classes are instantiated.
+
+The typical use cases for metaclasses:
+
+* logging;
+* registering classes at creation time;
+* interface checking;
+* automatically adding new methods;
+* automatically adding new variables.
+
+"""
